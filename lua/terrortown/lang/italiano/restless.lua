2f738648e594c62c654c0989a5ae487e107c3f5e
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-09-23 16:57:31 +0200

    Italian

---
+++ lua/terrortown/lang/italiano/restless.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+L[RESTLESS.name] = "Insonne"
+L[RESTLESS.defaultTeam] = "TEAM Insonni"
+L["hilite_win_" .. RESTLESS.defaultTeam] = "GLI INSONNI HANNO VINTO"
+L["win_" .. RESTLESS.defaultTeam] = "Gli Insonni hanno vinto!"
+L["info_popup_" .. RESTLESS.name] = [[Sei un Insonne! Devi uccidere tutti e tornerai in vita appena muorirai!]]
+L["body_found_" .. RESTLESS.abbr] = "Loro erano Insonni!"
+L["search_role_" .. RESTLESS.abbr] = "Questa persona era Insonne!"
+L["target_" .. RESTLESS.name] = "Insonne"
+L["ttt2_desc_" .. RESTLESS.name] = [[Insonne è un ruolo che continua a ritornare in vita più debole, ha vite limitate]]
+
+L["restless_reviving"] = "Tu hai {lives} vite rimanenti!"
+L["restless_final_life"] = "Questa è la tua vita finale!"
+L["ttt2_rst_target_mult"] = "Tu fai {multiplier}x di danno!"
